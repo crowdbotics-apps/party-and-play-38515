6df807e187ede45eb16da4a52c379d927cbbe29e
Crowdbotics <team@crowdbotics.com>
2023-03-01 05:02:08 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,226 +1,448 @@
-import React from "react";
-import { Text, StyleSheet, View, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+  Pressable
+} from "react-native";
 
-const EventsAdvancedScreen = () => {
+import { Slider } from "react-native-elements";
+
+const Filter = () => {
+  const [search, setSearch] = useState("");
+  const [selected, setSelected] = useState([]);
+  const [rangeStart, setRangeStart] = useState("");
+  const [rangeEnd, setRangeEnd] = useState("");
+  const [location, setLocation] = useState("");
+  const [age, setAge] = useState("");
+  const [grades, setGrades] = useState("");
+  const [interests, setInterests] = useState("");
+  const handleSelection = (title) => {
+    if (selected.includes(title)) {
+      setSelected(selected.filter((item) => item !== title));
+    } else {
+      setSelected([...selected, title]);
+    }
+  };
   return (
-    <ScrollView >
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Image source={require("./assets/back.png")} style={styles.back} />
-          <Text style={styles.heading}>Events</Text>
-          <View style={styles.IconContainer}>
-            <Image source={require("./assets/star.png")} style={styles.star} />
-            <Image source={require("./assets/settings.png")} style={styles.settings} />
-          </View>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Input
+          text="Filter"
+          value={search}
+          onChange={setSearch}
+          icon={require("./assets/filterIcon.png")}
+        />
+        <Separator title={"Desired Days"} />
+        <Tile
+          title={"Sunday"}
+          selected={selected.includes("Sunday")}
+          onPress={(x) => handleSelection(x)}
+        />
+        <Tile
+          title={"Wednesday"}
+          selected={selected.includes("Wednesday")}
+          onPress={(x) => handleSelection(x)}
+        />
+        <Separator title={"Desired Days"} />
+        <Tile
+          title={"Half-Day Mornins"}
+          selected={selected.includes("Half-Day Mornins")}
+          onPress={(x) => handleSelection(x)}
+        />
+        <Tile
+          title={"Full Day"}
+          selected={selected.includes("Full Day")}
+          onPress={(x) => handleSelection(x)}
+        />
+        <Separator title={"Budget"} />
+        <Tile
+          title={"Per week"}
+          selected={selected.includes("Per week")}
+          onPress={(x) => handleSelection(x)}
+        />
+        <View style={styles.halfInputs}>
+          <Input
+            text="From"
+            value={rangeStart}
+            onChange={setRangeStart}
+            containerStyle={styles.inputContainer}
+          />
+          <Input
+            text="To"
+            value={rangeEnd}
+            onChange={setRangeEnd}
+            containerStyle={styles.inputContainer}
+          />
         </View>
-        <Text style={styles.headingTxt}>Event type</Text>
-        <View style={styles.tabView}>
-          <View style={styles.tabItem}>
-            <Text>Party</Text>
-          </View>
-          <View style={styles.tabItem}>
-            <Text>Sports</Text>
-          </View>
-          <View style={[styles.tabItem, styles.selectedTab]}>
-            <Text>Cultural</Text>
-          </View>
-          <View style={styles.tabItem}>
-            <Text>Running</Text>
-          </View>
-        </View>
-
-        <View style={styles.mainContainer}>
-          <Text style={styles.subHeading}>Sponsored Events</Text>
-          <View style={styles.walletCard}>
-            <View style={styles.walletInner}>
-              <View style={styles.imgContainer}>
-                <Image source={require("./assets/edit.png")} style={styles.image} />
-              </View>
-
-              <View style={styles.walletCarder}>
-                <Text style={styles.eventName}>Event name</Text>
-                <Text style={styles.eventType}>Event type</Text>
-                <Text style={styles.attending}>Attending</Text>
-              </View>
-            </View>
-            <View style={styles.leftSection}>
-              <Text style={styles.date}>06.12.2022</Text>
-              <Text style={styles.time}>12:45 PM</Text>
-              <View style={styles.attendingContainer}>
-                <Text style={styles.people}>2723</Text>
-                <Image source={require("./assets/user.png")} style={styles.userImg} />
-              </View>
-            </View>
-          </View>
-        </View>
-        <View style={styles.myEvents}>
-          <View style={styles.myEventsHeading}>
-            <Text style={styles.myEventsText}>My event</Text>
-          </View>
-
-          <View style={styles.boxContainer}>
-            <View style={styles.checkboxContainer}>
-              <Text style={styles.label}>Yes</Text>
-              <Image source={require("./assets/checkbox.png")} style={styles.checkBox} />
-            </View>
-            <View style={styles.checkboxContainer}>
-              <Text style={styles.label}>No</Text>
-              <Image source={require("./assets/box.png")} style={styles.checkBox} />
-            </View>
-            <View style={styles.checkboxContainer}>
-              <Text style={styles.label}>Maybe</Text>
-              <Image source={require("./assets/box.png")} style={styles.checkBox} />
-            </View>
-          </View>
-        </View>
-        <View style={styles.centerBox}>
-          <View style={styles.box}>
-            <Image source={require("./assets/Vector.png")} style={styles.vector} />
-            <View style={styles.imageBox}>
-              <Image source={require("./assets/edit.png")} style={styles.editImg} />
-            </View>
-          </View>
-        </View>
-        <View style={styles.location}>
-          <Text>12:45 PM</Text>
-          <Text>Location</Text>
-        </View>
-        <View style={styles.tags}>
-          <Text style={styles.tagText}>Additional info</Text>
-          <Text style={[styles.tagText, styles.m15]}>Invites</Text>
-          <Text style={styles.tagText}>RSVP</Text>
-        </View>
-      </View>
-      <View style={styles.bottom}>
-        <Image source={require("./assets/download.png")} style={styles.star} />
-        <Image source={require("./assets/mos.png")} style={styles.star} />
-        <Image source={require("./assets/copy.png")} style={styles.star} />
-        <Image source={require("./assets/star.png")} style={styles.star} />
-        <Image source={require("./assets/contact.png")} style={styles.star} />
-      </View>
-    </ScrollView>
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          minimumTrackTintColor="#a1a1a1"
+          maximumTrackTintColor="#e6e6e6"
+          thumbTintColor="#000"
+          thumbStyle={styles.thumb}
+          step={1}
+          value={50}
+        />
+        <Separator title={"Activity"} />
+        <Input
+          text="Location"
+          value={location}
+          onChange={setLocation}
+          placeholder="10 miles"
+        />
+        <Input text="Age" value={age} onChange={setAge} />
+        <Input text="Grades" value={grades} onChange={setGrades} />
+        <Input text="Interests" value={interests} onChange={setInterests} />
+        <TabView
+          tabTitles={["Interest", "Interest", "Interest"]}
+          style={styles.tabs}
+          backgroundColor="#fff"
+          selected={0}
+          tabColor="#f1f1f1"
+        />
+        <Button buttonText="Apply" />
+      </ScrollView>
+    </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingHorizontal: 10
+    paddingHorizontal: 20
   },
-  headingTxt: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginLeft: 22
+  heading: {
+    fontSize: 20
   },
-  mainContainer: { width: "100%", marginVertical: 30 },
-  subHeading: { fontSize: 16, fontWeight: "bold", marginLeft: 22 },
-  tabView: {
-    width: "100%",
-    height: 48,
-    backgroundColor: "#F1F1F1",
+  halfInputs: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    marginTop: 20,
+    justifyContent: "space-between"
+  },
+  inputContainer: {
+    flex: 1,
+    marginHorizontal: 10
+  },
+  thumb: {
+    width: 20,
+    height: 20
+  },
+  tabs: {
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    width: "100%"
+  }
+});
+
+export default Filter;
+const Input = (props) => {
+  return (
+    <View style={[inputStyles.inputContainer, props.containerStyle]}>
+      {props.text
+        ? (
+        <Text style={inputStyles.inputText}>{props.text}</Text>
+          )
+        : null}
+
+      <TextInput
+        style={[
+          inputStyles.input,
+          props.style,
+          props.textArea ? inputStyles.textArea : null
+        ]}
+        placeholder={props.placeholder ? props.placeholder : "Enter"}
+        value={props.value}
+        onChangeText={(text) => props.onChange(text)}
+        placeholderTextColor={
+          props.placeholderTextColor ? props.placeholderTextColor : "#9B9B9B"
+        }
+        editable={props.editable !== false}
+        autoCapitalize="none"
+        autoCorrect={false}
+        multiline={!!props.textArea}
+      />
+      {props.errorText
+        ? (
+        <Text style={inputStyles.error}>{props.errorText}</Text>
+          )
+        : null}
+      {props.icon
+        ? (
+        <Image
+          source={props.icon}
+          style={
+            props.text ? inputStyles.iconWithText : inputStyles.iconWithoutText
+          }
+        />
+          )
+        : null}
+      <View style={styles.children}>{props.children}</View>
+    </View>
+  );
+};
+
+const inputStyles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: "center"
+    // flex: 1
+  },
+  inputText: {
+    fontSize: 14,
+    marginLeft: 20,
+    color: "#111112"
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    borderRadius: 10,
+    padding: 10,
+    paddingLeft: 20,
+    marginVertical: 10,
+    width: "100%",
+    height: 50
+  },
+  iconWithText: {
+    position: "absolute",
+    right: 30,
+    top: 48,
+    width: 15,
+    height: 15,
+    resizeMode: "contain"
+  },
+  iconWithoutText: {
+    position: "absolute",
+    right: 30,
+    top: 28,
+    width: 15,
+    height: 15,
+    resizeMode: "contain"
+  },
+  textArea: {
+    height: 150
+  },
+  children: {}
+});
+
+const TabView = ({
+  tabTitles,
+  selected,
+  onPress,
+  tabColor,
+  backgroundColor,
+  style
+}) => {
+  const tabColorStyle = {
+    backgroundColor: tabColor || "#fff"
+  };
+  const backgroundColorStyle = {
+    backgroundColor: backgroundColor || "#F1F1F1"
+  };
+  const propStyle = style || {};
+  return (
+    <View
+      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}
+    >
+      {tabTitles.map((title, index) => (
+        <Pressable
+          onPress={() => (onPress ? onPress(index) : null)}
+          style={
+            index === selected
+              ? [tabViewStyles.selected, tabColorStyle]
+              : [tabViewStyles.unSelected, backgroundColorStyle]
+          }
+          key={index}
+        >
+          <Text>{title}</Text>
+        </Pressable>
+      ))}
+    </View>
+  );
+};
+
+const tabViewStyles = StyleSheet.create({
+  paletteContainer: {
+    width: "80%",
+    height: 48,
+    backgroundColor: "#E4E4E4",
+    flexDirection: "row",
     alignItems: "center",
     borderRadius: 10,
-    paddingVertical: 6,
-    paddingHorizontal: 6,
-    marginTop: 10
+    padding: 6,
+    marginVertical: 10
   },
-  tabItem: {
+  selected: {
+    borderRadius: 10,
+    flex: 1,
+    backgroundColor: "#fff",
     height: "100%",
-    paddingHorizontal: 8,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F1F1F1",
-    borderRadius: 10,
-    fontWeight: "bold",
-    flex: 1
-  },
-  selectedTab: {
-    backgroundColor: "#FFF",
     shadowColor: "gray",
     elevation: 10
   },
-  walletCard: {
-    backgroundColor: "#fff",
-    padding: 10,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginTop: 10,
-    borderRadius: 8,
-    elevation: 15,
-    shadowColor: "#ccc9c9"
-  },
-  walletInner: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  walletCarder: {
-    alignSelf: "center",
-    display: "flex",
-    flexDirection: "column"
-  },
-  eventName: {
-    color: "#000",
-    fontSize: 14,
-    marginLeft: 10,
-    width: 115
-  },
-  eventType: {
-    color: "#000",
-    fontSize: 12,
-    marginLeft: 10,
-    width: 115,
-    marginVertical: 5
-  },
-  attendingContainer: { flexDirection: "row", justifyContent: "center", alignItems: "center" },
-  userImg: { height: 15, width: 15, resizeMode: "contain" },
-  date: { fontSize: 14, color: "#000" },
-  time: { fontSize: 14, color: "#dadada", marginVertical: 5 },
-  people: { fontSize: 12, color: "#000" },
-  attending: { color: "#000", fontSize: 12, marginLeft: 10, width: 115 },
-  imgContainer: {
-    height: 80,
-    width: 80,
+  unSelected: {
+    flex: 1,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#dadada",
+    backgroundColor: "#E4E4E4",
     borderRadius: 10
-  },
-  image: { resizeMode: "contain" },
-  leftSection: { justifyContent: "flex-end", alignItems: "flex-end" },
-  checkboxContainer: {
-    flexDirection: "row",
-    marginBottom: 20
-  },
-  checkbox: {
-    alignSelf: "center"
-  },
-  myEvents: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
-  boxContainer: { flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", width: 250 },
-  checkBox: { height: 18, width: 18 },
-  myEventsHeading: { justifyContent: "center", alignItems: "center" },
-  myEventsText: { fontSize: 16, fontWeight: "bold", marginLeft: 30, marginBottom: 20 },
-  label: { marginRight: 5, fontSize: 14 },
-  box: { height: 170, width: 347, backgroundColor: "#fdf1d6", borderRadius: 10, alignItems: "center", justifyContent: "center" },
-  imageBox: { height: 150, alignItems: "center", justifyContent: "center" },
-  vector: { height: 14, width: 14, alignSelf: "flex-end", marginRight: 10, marginTop: 10, resizeMode: "contain" },
-  editImg: { marginBottom: 15 },
-  centerBox: { justifyContent: "center", alignItems: "center" },
-  location: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginHorizontal: 45, marginVertical: 10 },
-  tags: { flexDirection: "row", justifyContent: "flex-start", alignItems: "center", marginHorizontal: 30, marginTop: 20, marginBottom: 50 },
-  tagText: { paddingVertical: 6, paddingHorizontal: 10, backgroundColor: "#F1F1F1", borderRadius: 12 },
-  m15: { marginHorizontal: 15 },
-  header: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginHorizontal: 30, marginTop: 10, marginBottom: 40 },
-  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -20 },
-  heading: { fontSize: 16, color: "#000", marginLeft: 20 },
-  star: { width: 20, height: 20, resizeMode: "contain" },
-  settings: { width: 20, height: 19.4, resizeMode: "contain" },
-  IconContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: 55, marginRight: -20 },
-  bottom: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingVertical: 20, paddingHorizontal: 20, borderTopWidth: 1, borderTopColor: "#000", backgroundColor: "#fff" }
+  }
 });
 
-export default EventsAdvancedScreen;
+const Separator = ({ title }) => {
+  return (
+    <View style={separatorStyles.separator}>
+      <Text style={separatorStyles.separatorText}>{title}</Text>
+      <TabView
+        tabTitles={["Choose"]}
+        selected={0}
+        style={separatorStyles.tabView}
+      />
+    </View>
+  );
+};
+
+const separatorStyles = StyleSheet.create({
+  separator: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10
+  },
+  separatorText: {
+    fontSize: 16,
+    color: "#12D790",
+    flex: 1
+  },
+  tabView: {
+    width: 120,
+    marginVertical: 0
+  }
+});
+
+const Checkbox = (props) => {
+  return (
+    <Pressable
+      onPress={() => {
+        props.setValue(!props.value);
+      }}
+      style={[checkboxStyles.container, props.style]}
+    >
+      <Image
+        source={
+          props.value
+            ? require("./assets/checkboxIconActive.png")
+            : require("./assets/checkboxIcon.png")
+        }
+        style={[checkboxStyles.checkbox]}
+      />
+    </Pressable>
+  );
+};
+
+const checkboxStyles = StyleSheet.create({
+  container: {
+    height: 20,
+    width: 20
+  },
+  checkbox: {
+    height: "100%",
+    width: "100%"
+  }
+});
+
+const Tile = ({ title, onPress, selected }) => {
+  return (
+    <View style={tileStyles.container}>
+      <Text style={tileStyles.title}>{title}</Text>
+      <Checkbox
+        style={tileStyles.checkbox}
+        value={selected}
+        setValue={() => onPress(title)}
+      />
+    </View>
+  );
+};
+
+const tileStyles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderBottomColor: "#e6e6e6",
+    borderBottomWidth: 1
+  },
+  title: {
+    fontSize: 16,
+    color: "#111112"
+  }
+});
+const Button = (params) => {
+  const backgroundColor = params.color ? params.color : "#000";
+  const textColor = params.textColor ? params.textColor : "#fff";
+  const btnStyle = {
+    backgroundColor: params.outline ? "#fff" : backgroundColor,
+    borderColor: params.outline ? backgroundColor : null,
+    borderWidth: params.outline ? 1 : 0
+  };
+  const btnText = {
+    color: params.outline ? "#000" : textColor
+  };
+  return (
+    <View style={buttonStyles.btnContainer}>
+      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
+        <Pressable
+          style={[buttonStyles.btn, btnStyle, params.style]}
+          onPress={params.onPress}
+        >
+          <Text style={[buttonStyles.btnText, btnText]}>
+            {params.buttonText}
+          </Text>
+          <View style={styles.childrenContainer}>{params.children}</View>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const buttonStyles = StyleSheet.create({
+  btnContainer: {
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    marginVertical: 20
+  },
+  shadowContainer: {
+    shadowColor: "rgba(0, 0, 0, 0.5)",
+    elevation: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10
+  },
+  btn: {
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+
+    flexDirection: "row"
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  childrenContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
