78c3300991420a8c1fdcb77e53b4ce212f3ddec2
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-03-28 17:19:28 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/BlankScreen17798/index.js
+++ screens/BlankScreen17798/index.js
@@ -1,9 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const BlankScreen17798 = () => {
+  const navigation = useNavigation();
   return <View style={_styles.BlankScreen17798}>
-      </View>;
+      <Pressable onPress={() => {
+      navigation.navigate("login");
+    }}><Text style={_styles.KbnasoWT}>Login </Text></Pressable></View>;
 };
 
 export default BlankScreen17798;
@@ -15,5 +21,12 @@ const _styles = StyleSheet.create({
     position: "relative",
     height: "100%",
     borderRadius: 5
+  },
+  KbnasoWT: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
